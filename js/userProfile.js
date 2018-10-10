b37dbcb094ae1a1c00eb9b2c6fcc169cb1d98ce1
Ashley Elder <ashley.elder@techtonic.com>
2018-10-10 10:39:52 -0600

    adds array

--- js/userProfile.js
+++ js/userProfile.js
@@ -13,23 +13,13 @@
 //function takes in array of recognition objects
 //calls filterList to dispay
 
-function Recognition(message, username, avatar, date)
-{
-  this.message = message;
-  this.username = username;
-  this.avatar = avatar;
-  this.date = date;
-};
-
-//test
-
-// var recognition1 = new Recognition("great job turning in those tps reports on time", "Billy Bob", images/demoUser1.png, "October 1, 2018");
-
-var recognitionArray = [
-  {message: "blah", username: "blah", avatar: "demoUser1.png", date: "January 1, 2018"},
-  {message: "blah", username: "blah", avatar: "demoUser1.png", date: "January 1, 2018"},
-  {message: "blah", username: "blah", avatar: "demoUser1.png", date: "January 1, 2018"}
-];
-Recognition.prototype.getRecentRecognition = function(){
-  // var recentRecognitionsArray = [recognition1, recognition2, recognition3, recognition4, recognition5];
+function getRecentRecognition(arr){
+  var arr = [
+    {avatar:"demoUser1.png", username:"Shambre SW", date:"1 day ago", message:"Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit."},
+    {avatar:"demoUser1.png", username:"Erik H", date:"4 days ago", message:"Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit."},
+    {avatar:"demoUser1.png", username:"Jason D", date:"2 weeks ago", message:"Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit."},
+    {avatar:"demoUser1.png", username:"Shambre SW", date:"1 day ago", message:"Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit."},
+    {avatar:"demoUser1.png", username:"Shambre SW", date:"1 day ago", message:"Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit."}
+  ];
+  displayList(arr);
 };
