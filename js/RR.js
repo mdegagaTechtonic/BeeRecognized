7f288293af2d169a66f1b9bbe42baf2806bc0b45
Merry Degaga <merry.degaga@techtonic.com>
2018-10-10 16:49:49 -0600

    created skeleton for the filter click event, skeleton for the getAllRecognitionSent and getAllRecognitionReceived

--- js/RR.js
+++ js/RR.js
@@ -1,9 +1,25 @@
+var arr = getAllRecognitionReceived();
+
 $('document').ready(function(init){
   // defaultUserProfilePage();
   // defaultLoadPageRS();
   // defaultLoginPage();
-  getAllRecognitionReceived();
-  displayList();
+  //getAllRecognitionReceived();
+  displayList(arr);
   logout();
 
 });
+
+
+$("#filter").on("click", function(event) {
+  filter(event, arr);
+});
+
+function filter(event, arr) {
+  event.preventDefault();
+
+}
+
+function getAllRecognitionReceived() {
+  //will return all user objects that received a recognition from the user from the slack db?
+}
--- js/RS.js
+++ js/RS.js
@@ -1,12 +1,24 @@
+var arr = getAllRecognitionSent();
+
 $('document').ready(function(init){
   // defaultUserProfilePage();
   // defaultLoadPageRR();
   // defaultLoginPage();
   getUserNames();
   getDateRange();
-  displayList();
   getAllRecognitionSent();
   displayList(arr);
-  
-
 });
+
+$("#filter").on("click", function(event) {
+  filter(event, arr);
+});
+
+function filter(event, arr) {
+  event.preventDefault();
+
+}
+
+function getAllRecognitioSent() {
+  //will return all user objects that sent user a recogniton from the slack db?
+}
