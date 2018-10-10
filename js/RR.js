83320fd76530b62f0098dbc88bbdd152d0c42732
Yahor Nikifarau <>
2018-10-10 10:37:02 -0600

    init() added for all pages

---
+++ js/RR.js
@@ -0,0 +1,9 @@
+$('document').ready(function(init){
+  // defaultUserProfilePage();
+  // defaultLoadPageRS();
+  // defaultLoginPage();
+  getAllRecognitionReceived();
+  displayList();
+  logout();
+
+});
