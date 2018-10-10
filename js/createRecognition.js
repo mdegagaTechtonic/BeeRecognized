17e66f36283ee51986358fcae06e0864ee10fb69
Erik Hoy <>
2018-10-10 14:58:06 -0600

    added localstorage to updateDB function

--- js/createRecognition.js
+++ js/createRecognition.js
@@ -1,11 +1,12 @@
 $("form").submit(function(event) {
-  newArr = ($(this).serializeArray());
+  var msgArr = ($(this).serializeArray());
   event.preventDefault();
-  $.each(newArr,function(i,value) {
-    alert(value.value);
+  $.each(msgArr,function(i,value) {
+    // alert(value.value);
   });
   $(this).find("input[type=text],textarea").val("");
   updateBeesToGive();
+  updateDB(msgArr);
 });
 
 function updateBeesToGive() {
@@ -13,5 +14,31 @@ function updateBeesToGive() {
   var beesToGive = 5;
   var updatedBeesToGive = 5-1;
   //put updated bees to give
-  console.log(updatedBeesToGive);
+  // console.log(updatedBeesToGive);
 }
+
+function updateDB(msgArr) {
+  setLocalStorage(msgArr);
+}
+
+function setLocalStorage(msgArr) {
+  var stringifiedArr = JSON.stringify(msgArr);
+  localStorage.setItem('msgArr',stringifiedArr);
+}
+
+function getLocalStorage() {
+  var myArr = JSON.parse(window.localStorage.getItem('msgArr'));
+  if (myArr !== null) {
+    if (myArr.length > 0) {
+      for (var i = 0; i < myArr.length; i++) {
+        // new Book(myArr[i].title,myArr[i].author,myArr[i].numPages,myArr[i].pubDate);
+      }
+    }
+    var msgArr = myArr;
+  }
+  return msgArr;
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  this.newArr = getLocalStorage();
+});
