1a3038eb8b013cd16bc3bc99d519fcaf237f511c
Ashley Elder <ashley.elder@techtonic.com>
2018-10-11 10:12:51 -0600

    updates recognition object on userProfile

--- js/userProfile.js
+++ js/userProfile.js
@@ -11,26 +11,29 @@ $(document).ready(function(init){
   // displayList();
 });
 
-function Recognition(avatar, username, date, message)
+function Recognition(avatar, sender, receiver, beesToGive, date, message)
 {
   this.avatar = avatar;
-  this.username = username;
+  this.sender = sender;
+  this.receiver = receiver;
+  this.updateBeesToGive = beesToGive;
   this.date = date;
   this.message = message;
 };
 
-recognition1 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition2 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition3 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition4 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition5 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition6 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition7 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition8 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition9 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition10 = new Recognition("demoUser1.png", "Shambre SW", "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition1 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition2 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition3 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition4 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition5 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition6 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition7 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition8 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition9 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition10 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
+recognition11 = new Recognition("demoUser1.png", "Shambre SW", "Erik H", 5, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
 
-var recognitionArray = [recognition1, recognition2, recognition3, recognition4, recognition5, recognition6, recognition7, recognition8, recognition9, recognition10,];
+var recognitionArray = [recognition1, recognition2, recognition3, recognition4, recognition5, recognition6, recognition7, recognition8, recognition9, recognition10, recognition11];
 
 function getRecentRecognition(recognitionArray){
   var count = 0;
