71a5ef185cab53bc88f5a56693e1732a0bc17ee0
Erik Hoy <>
2018-10-11 14:16:16 -0600

    updated files to use new mockDB file

--- js/RR.js
+++ js/RR.js
@@ -2,34 +2,16 @@ $(document).ready(function(init){
   // defaultUserProfilePage();
   // defaultLoadPageRS();
   // defaultLoginPage();
-  getAllRecognitionReceived(recognitionReceived);
-  displayList();
+  getAllRecognitionReceived();
+  // displayList();
   // logout();
 });
 
-function Recognition(avatar, sender, receiver, beesToGive, date, message)
-{
-  this.avatar = avatar;
-  this.sender = sender;
-  this.receiver = receiver;
-  this.updateBeesToGive = beesToGive;
-  this.date = date;
-  this.message = message;
-};
-
-recognition1 = new Recognition("demoUser2.png", "Ashley E", "Merry D", 10, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition2 = new Recognition("demoUser2.png", "Ashley E", "Merry D", 10, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition3 = new Recognition("demoUser2.png", "Ashley E", "Merry D", 10, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition4 = new Recognition("demoUser2.png", "Ashley E", "Merry D", 10, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-recognition5 = new Recognition("demoUser2.png", "Ashley E", "Merry D", 10, "1 day ago", "Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.")
-
-var recognitionReceived = [recognition1, recognition2, recognition3, recognition4, recognition5];
-
-function getAllRecognitionReceived(recognitionReceived){
-  var numberReceived = recognitionReceived.length;
+function getAllRecognitionReceived(){
+  var numberReceived = recognition.length;
   beeCount = "Total: " + numberReceived;
   $(".total").attr("title","Total: " + numberReceived);
   $(".total").append(beeCount);
-  displayList(recognitionReceived);
-  return recognitionReceived;
+  displayList(recognition, "RR");
+  // return recognitionReceived;
 };
