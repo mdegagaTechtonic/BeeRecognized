9526a23a2e04412b2141b30f21f09b3f047bd719
Erik Hoy <>
2018-10-12 13:59:27 -0600

    updated displaylist to account for empty array

--- js/displayList.js
+++ js/displayList.js
@@ -3,15 +3,17 @@ function displayList(arr, page) {
   $(".recognition").html("");
   $.each(arr, function(index, value) {
     var newContent = "<div class='d-flex w-100 justify-content-between'><div class='item'>";
-    if (page === "RR" || page === "SB") {
-      newContent += "<img src='images/"+value.avatarSender+"' alt='slack user icon' class='userImage' width='100'>";
-      newContent += "<h5 class='mt-2 mb-2'>"+value.sender+"</h5>";
-    } else {
-      newContent += "<img src='images/"+value.avatarReceiver+"' alt='slack user icon' class='userImage' width='100'>";
-      newContent += "<h5 class='mt-2 mb-2'>"+value.receiver+"</h5>";
+    if (value.avatarSender) {
+      if (page === "RR" || page === "SB") {
+        newContent += "<img src='images/"+value.avatarSender+"' alt='slack user icon' class='userImage' width='100'>";
+        newContent += "<h5 class='mt-2 mb-2'>"+value.sender+"</h5>";
+      } else {
+        newContent += "<img src='images/"+value.avatarReceiver+"' alt='slack user icon' class='userImage' width='100'>";
+        newContent += "<h5 class='mt-2 mb-2'>"+value.receiver+"</h5>";
+      }
+      newContent += "</div><div class='row'>";
+      newContent += "<small class='text-muted mr-2'>"+value.date+"</small>";
     }
-    newContent += "</div><div class='row'>";
-    newContent += "<small class='text-muted mr-2'>"+value.date+"</small>";
     newContent += "</div></div>";
     newContent += "<p class='mt-1 mb-1'>"+value.message+"</p>";
     $(".recognition").append(newContent);
