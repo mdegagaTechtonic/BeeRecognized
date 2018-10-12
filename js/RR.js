ad4616f4ae693da07dc6fffc98d25b5a8aedb104
Ashley Elder <ashley.elder@techtonic.com>
2018-10-12 09:09:00 -0600

    completes jquery js getAllRR function with local storage as db

--- js/RR.js
+++ js/RR.js
@@ -2,23 +2,11 @@ $(document).ready(function(init){
   // defaultUserProfilePage();
   // defaultLoadPageRS();
   // defaultLoginPage();
-  // getName();
   getAllRecognitionReceived();
   // displayList();
   updateBeesReceived();
   // logout();
 });
-
-function Recognition(avatarSender, avatarReceiver, sender, receiver, beesToGive, date, message)
-{
-  this.avatarSender = avatarSender;
-  this.avatarReceiver = avatarReceiver;
-  this.sender = sender;
-  this.receiver = receiver;
-  this.updateBeesToGive = beesToGive;
-  this.date = date;
-  this.message = message;
-};
 
 function getName(event){
   var username = $('span.username').text().slice(0, -2);//remove 's from username
@@ -26,13 +14,13 @@ function getName(event){
 };
 
 function getAllRecognitionReceived(){
-  // localStorage = localStorage.getItem("db");//mock db call
+  var db = JSON.parse(localStorage.getItem("db"));//mock db call
   username = getName();
-  console.log(username);
-  receivedRecognition = [];
-  for (var i = 0; i < recognition.length; i++) {
-    if (recognition[i].receiver.toLowerCase() === username.toLowerCase()) {//when localstorage works replace var recognition with var localstorage
-      receivedRecognition.push(recognition[i]);
+  var receivedRecognition = [];
+  for (var i = 0; i < db.length; i++) {
+    if (db[i].receiver.toLowerCase() === username.toLowerCase()) {//when localstorage works replace var recognition with var localstorage
+      receivedRecognition.push(db[i]);
+      console.log(receivedRecognition);
     }
   }
   displayList(receivedRecognition, "RR");
